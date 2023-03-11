20a39999203bc8aa146eaa0702137a3b24c00469
bisht-xp <bishtkamal2907@gmail.com>
2023-03-11 10:26:56 +0530

    created constants folder

---
+++ constants/index.tsx
@@ -0,0 +1,23 @@
+interface links {
+  id: string;
+  title: string;
+}
+
+export const navLinks: links[] = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
