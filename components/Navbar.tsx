6ff50404d3130f75b3f02825a9eee82b1bb3f717
bisht-xp <bishtkamal2907@gmail.com>
2023-03-11 14:10:12 +0530

    Finished navbar functionality and styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,58 @@ import { navLinks } from "../constants";
 const Navbar = () => {
   const [toggle, setToggle] = useState<boolean>(false);
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
+    <>
+      <nav className="w-full flex py-6 justify-between items-center navbar">
+        <Link href={`#`} className="w-[124px] h-[32px] text-white text-2xl">
+          CoinWatch
+        </Link>
+        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                index === navLinks.length - 1 ? "mr-0" : "mr-10"
+              }`}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? "/assets/close.svg" : "/assets/menu.svg"}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          >
+            <ul className="list-none flex justify-end items-center flex-1 flex-col">
+              {navLinks.map((nav, index) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] text-white hover:text-dimWhite ${
+                    index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                  }`}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
+
+{
+  /* <nav className="w-full flex py-6 justify-between items-center navbar">
       <Link href={`#`} className="w-[124px] h-[32px] text-white text-2xl">
         CoinWatch
       </Link>
@@ -47,8 +98,5 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-    </nav>
-  );
-};
-
-export default Navbar;
+    </nav> */
+}
